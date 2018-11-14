1d8dccba493e591670e0b7311a77c6ddf78b07f6
Geová Ramalho dos Santos <gevsantos@hotmail.com>
2018-11-14 18:13:43 -0200

    Migração do AdminLTE TPL e Criação do PageAdmin

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
  require_once("vendor/autoload.php");
  use \Slim\Slim;
  use \GeovaRS\Page;
+ use \GeovaRS\PageAdmin;
+
  $app = new Slim();
  $app->config('debug', false);
  $app->get
@@ -9,6 +11,15 @@
   function()
   {
    $page = new Page();
+   $page->setTpl("index");
+  }
+ );
+
+ $app->get
+ ('/admin',
+  function()
+  {
+   $page = new PageAdmin();
    $page->setTpl("index");
   }
  );
