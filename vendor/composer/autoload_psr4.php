a1b1588c495cdf5a3ae68fc8a4e6aaa25c6f4860
Geová Ramalho dos Santos <gevsantos@hotmail.com>
2018-11-15 15:09:52 -0200

    CRUD de Usuários para LogIn

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'GeovaRS\\' => array($vendorDir . '/GeovaRS/php-classes/src'),
+ 'GeovaRS\\' => array($vendorDir . '/GeovaRS/php-classes/src'),
 );
